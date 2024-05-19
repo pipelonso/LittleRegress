f36186016f37b3aea3373dae43690e960baf5a35
pipelonso <andresfelipeibanezcuta2@gmail.com>
2024-05-19 01:44:17 -0500

    Starts  of armor editor

--- core/framecraft/tools/armorCreator.py
+++ core/framecraft/tools/armorCreator.py
@@ -1,13 +1,33 @@
 from ursina import *
 
-app = Ursina()
-
-cubo =  Entity(model='cube', color=hsv(300,1,1), scale=2, collider='box')
+app = Ursina(borderless=False)
 
 
-Sky(Texture="sky_sunset.jpg")
+class Voxel(Button):
+    def __init__(self, position=(0,0,0), message = ""):
+        super().__init__(parent=scene,
+            position=position,
+            model='plane',
+            origin_y=.5,
+            color=color.red,
+            highlight_color=color.lime,
+            message = message
+        )
 
 
+
+def sayHola(cubo):
+    print(cubo.message)
+
+cubo = Voxel(position=(1,0,0), message = "Puto el que lo lea")
+
+cubotwo = Voxel(position=(2,0,0), message = "Te amo reisy")
+
+cubo.on_click = Sequence(Func(sayHola, cubo))
+cubotwo.on_click = Sequence(Func(sayHola, cubotwo))
+
+
+Sky(texture="sky_sunset")
 
 
 
